fe5d9600248af7d338ccf2ea726902704c1e9f73
FedoraLord <jerkrouse@gmail.com>
2018-01-26 21:54:02 -0600

    Editing player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+    public Rigidbody2D rb;
+    public float maxSpeed;
+    public float acceleration;
+    public float resistance;
+    public float rotateSpeed;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Accelerate();
+        }
+        else
+        {
+            Deccelerate();
+        }
+    }
+
+    private void Accelerate()
+    {
+        Vector2 add = transform.up * acceleration / 100;
+        rb.velocity += add;
+    }
+
+    private void Deccelerate()
+    {
+        rb.velocity = rb.velocity * 0.9f;
+    }
 }
