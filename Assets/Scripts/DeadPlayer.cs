8b0db213d8eb9a601ad1ece753d3d4b427c91eab
FedoraLord <jerkrouse@gmail.com>
2018-01-28 13:54:42 -0600

    everything

---
+++ Assets/Scripts/DeadPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadPlayer : MonoBehaviour {
+
+    public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb.angularVelocity = 25;
+        rb.velocity = new Vector2(1.7f, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -91,6 +91,7 @@ public class Player : MonoBehaviour {
         {
             MaxEnergy();
             Destroy(collision.gameObject);
+            gc.ObjectiveReached();
         }
     }
 
